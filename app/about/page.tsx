baff7986d32cfe54560d380cd89b93968f4fe9ba
Anand Kumar Mishra <ctfu.anand@outlook.com>
2024-08-01 21:08:15 +0530

    chore: Update avatar fallback to display username instead of initials

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -22,7 +22,7 @@ export default async function AboutPage() {
         <div className="min-w-48 max-w-48 flex flex-col gap-2">
           <Avatar className="h-48 w-48">
             <AvatarImage src="https://avatars.githubusercontent.com/u/110248822?v=4" alt={siteConfig.author} />
-            <AvatarFallback>JC</AvatarFallback>
+            <AvatarFallback>akm-xdd</AvatarFallback>
           </Avatar>
           <h2 className="text-2xl font-bold text-center break-words">
             {siteConfig.author}
